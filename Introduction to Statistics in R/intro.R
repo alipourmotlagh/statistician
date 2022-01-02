3b6795bcc643213650bce8ad9fafef583fa5d0f2
alipourmotlagh <alipourmotlagh@gmail.com>
2022-01-02 17:47:55 +0100

    3
    a

---
+++ Introduction to Statistics in R/intro.R
@@ -0,0 +1,9 @@
+library(dplyr)
+library(ggplot2)
+library(tidyr)
+library(tidyverse)
+library(data.table)
+food_consumption<-readRDS("food_consumption.rds")
+seller_1<-readRDS("seller_1.rds")
+
+world_happiness_sugar<-readRDS("world_happiness_sugar.rds")
